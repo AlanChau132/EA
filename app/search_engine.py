755d78174f733413bd0d8b51deedf8c7e290cfdf
oscarntw <143982016+oscarntw@users.noreply.github.com>
2024-04-23 11:03:10 +0800

    delete search_engine

--- app/search_engine.py
+++ app/search_engine.py
@@ -1,19 +1,1 @@
-from flask import Flask, render_template, request
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/search', methods=['POST'])
-def search():
-    query = request.form['query']
-  
- 
-    search_results = ['Result 1', 'Result 2', 'Result 3']
-    
-    return render_template('results.html', query=query, results=search_results)
-
-if __name__ == '__main__':
-    app.run(debug=True)
